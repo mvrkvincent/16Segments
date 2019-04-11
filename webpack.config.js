0c35c5ff1dc42bc33450c17e96dacbb133df718c
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-04-11 13:12:40 -0400

    Configure webpack and d3

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            presets: ['@babel/env']
+          }
+        },
+      },
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig]
+}
