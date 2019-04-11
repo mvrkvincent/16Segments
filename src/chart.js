99c4469b812565195ae80497b765735fd1e8f343
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-04-11 16:26:26 -0400

    Begin full buildout of chart

--- src/chart.js
+++ src/chart.js
@@ -4,17 +4,21 @@ const Graph = () => {
   
   const data = testData;
 
-  const d = 400;
-  const r = (d / 2);
+  const dia = 400;
+  const rad = (dia / 2);
   const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
   
   const graph = d3.select('svg')
-  .attr({'width': d, 'height': d})
+  .attr({'width': dia, 'height': dia})
   .append('g')
-  .attr('transform', 'translate(' + rad + ',' + rad + ')');
+  .attr('transform', 'translate(' + rad+ ',' + rad + ')');
   
-  const partition = d3.partition().size(2 * Math.PI, r);
+  const partition = d3.partition().size(2 * Math.PI, rad);
+  const root = d3.hierarchy(data)
+    .sum(d => d.size)
+    .sort(a,b => b.value - a.value)
 
+  
 
 
 }
