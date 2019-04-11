f3d86271b3449bb58eda0629855b2ac556363ea9
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-04-11 18:52:34 -0400

    Complete basic graph functionality

--- src/data.js
+++ src/data.js
@@ -1,19 +1,48 @@
 const testData = {
-  "name": "Expenses", 
-  "children": [{ 
-      "name": "Food", 
-      "children": [{ "name": "At Home", "cost": 4 }, { "name": "Away", "cost": 4}]
-    }, {
-      "name": "Housing",
-      "children": [{ "name": "Shelter", "cost": 3 }, { "name": "Utilities", "cost": 3 }, {"name": "Expenses", "cost": 3}]
-    }, {
-      "name": "Clothing",
-      "children": [{ "name": "Apparal", "cost": 4 }, { "name": "Shoes", "cost": 4 }, { "name": "Coats", "cost": 2 }, { "name": "Accessories", "cost": 1 }]
-    }, {
-      "name": "Healthcare",
-      "children": [{ "name": "Insurance", "cost": 4 }, { "name": "Medication", "cost": 4 }, { "name": "Expenses", "cost": 3 }]
-    }, {
-      "name": "Entertainment",
-      "children": [{ "name": "Tickets", "cost": 4 }, { "name": "Pets", "cost": 4 }]
-    }]
-  };
+  'category': 'Expenses', 'children': [{
+    'category': 'Food',
+    'children': [{ 'category': 'sub', 'cost': 5 }, { 'category': 'sub2', 'cost': 5 }]
+  }, {
+    'category': 'Alcohol',
+    'cost': 3
+  }, {
+    'category': 'Housing',
+    'children': [{ 'category': 'sub', 'cost': 3 }, { 'category': 'sub2', 'cost': 3 }, { 'category': 'sub3', 'cost': 3 }, { 'category': 'sub4', 'cost': 3 }, { 'category': 'sub5', 'cost': 3 }]
+  }, {
+    'category': 'Apparel & Services',
+    'children': [{ 'category': 'sub', 'cost': 3 }, { 'category': 'sub2', 'cost': 3 }, { 'category': 'sub3', 'cost': 3 }, { 'category': 'sub4', 'cost': 3 }, { 'category': 'sub5', 'cost': 3 }]
+  }, {
+    'category': 'Transportation',
+    'children': [{ 'category': 'sub', 'cost': 4 }, { 'category': 'sub2', 'cost': 4 }, { 'category': 'sub3', 'cost': 4 }, { 'category': 'sub4', 'cost': 4 }]
+  }, {
+    'category': 'Healthcare',
+    'children': [{ 'category': 'sub', 'cost': 4 }, { 'category': 'sub2', 'cost': 4 }, { 'category': 'sub3', 'cost': 4 }, { 'category': 'sub4', 'cost': 4 }]
+  }, {
+    'category': 'Entertainment',
+    'children': [{ 'category': 'sub', 'cost': 4 }, { 'category': 'sub2', 'cost': 4 }, { 'category': 'sub3', 'cost': 4 }, { 'category': 'sub4', 'cost': 4 }]
+  }, {
+    'category': 'Personal Care',
+    'cost': 4
+  }, {
+    'category': 'Reading',
+    'cost': 2
+  }, {
+    'category': 'Education',
+    'cost': 5
+  }, {
+    'category': 'Tobacco',
+    'cost': 1
+  }, {
+    'category': 'Misc.',
+    'cost': 4
+  }, {
+    'category': 'Cash',
+    'cost': 4
+  }, {
+    'category': 'Taxes',
+    'cost': 5
+  }, {
+    'category': 'Personal Insurance & Pensions',
+    'children': [{ 'category': 'sub', 'cost': 1 }, { 'category': 'sub2', 'cost': 4 }]
+  }]
+};
--- src/graph.js
+++ src/graph.js
@@ -1,56 +1,36 @@
-// import { testData } from './data';
-
-
-testData = {
-  "name": "Expenses",
-  "children": [{
-    "name": "Food",
-    "children": [{ "name": "At Home", "cost": 4 }, { "name": "Away", "cost": 4 }]
-  }, {
-    "name": "Housing",
-    "children": [{ "name": "Shelter", "cost": 3 }, { "name": "Utilities", "cost": 3 }, { "name": "Expenses", "cost": 3 }]
-  }, {
-    "name": "Clothing",
-    "children": [{ "name": "Apparal", "cost": 4 }, { "name": "Shoes", "cost": 4 }, { "name": "Coats", "cost": 2 }, { "name": "Accessories", "cost": 1 }]
-  }, {
-    "name": "Healthcare",
-    "children": [{ "name": "Insurance", "cost": 4 }, { "name": "Medication", "cost": 4 }, { "name": "Expenses", "cost": 3 }]
-  }, {
-    "name": "Entertainment",
-    "children": [{ "name": "Tickets", "cost": 4 }, { "name": "Pets", "cost": 4 }]
-  }]
-};
-
 const data = testData;
 
-const dia = 400;
-const rad = (dia / 2);
+const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1));
+
+var dia = 400;
+var rad = dia / 2;
 
 const graph = d3.select('svg')
-  .attr({'width': dia, 'height': dia})
+  .attr('width', dia)
+  .attr('height', dia)
   .append('g')
-  .attr('transform', 'translate(' + rad+ ',' + rad + ')');
+  .attr('transform', 'translate(' + rad + ',' + rad + ')');
 
-const partition = d3.partition().size(2 * Math.PI, rad);
+const partition = d3.partition().size([2 * Math.PI, rad]);
+
 const root = d3.hierarchy(data)
-  .sum(d => d.size)
-  .sort(a,b => b.value - a.value);
+  .sum(d => d.cost)
+  .sort((a, b) => b.value - a.value);
+
+partition(root);
 
 const arc = d3.arc()
   .innerRadius(d => d.y0)
   .outerRadius(d => d.y1)
   .startAngle(d => d.x0)
-  .endAngle(d.x1);
-
-const color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1));
-
-partition(root);
+  .endAngle(d => d.x1);
 
 graph.selectAll('path')
-  .enter('path')
+  .data(root.descendants())
+  .enter()
+  .append('path')
+  .attr('display', d => d.depth ? null : 'none')
   .attr('d', arc)
-  .style({
-    'stroke': white,
-    'fill': d => color((d.children ? d : d.parent).data.name)
-  });
+  .style('stroke', '#fff')
+  .style('fill', d => color((d.children ? d : d.parent).data.category));
 
