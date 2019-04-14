9141d2b4ee460eae087d2136d6377a5fa1aa3927
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-04-14 18:48:15 -0400

    Minor styling adjustments to nav

--- src/ledger.js
+++ src/ledger.js
@@ -1,16 +1,19 @@
 const buildLedger = (data, root) => {
 
   let ledgerItems = [];
-
-
+  let total = 0;
+  
   for (let i = 0; i < root.children.length; i++) {
     const ledgerVal = root.children[i].value;
     ledgerItems.push(`<li class="ledger-item">
-                          <div class="value">${ledgerVal}</div>
-                          <span>${data.children[i].category}_</span>
-                      </li>`)
-  }
+    <div class="value">${ledgerVal}</div>
+    <span>${root.children[i].data.category}_</span>
+    </li>`);
+    
+    total += ledgerVal;                  
+  };
+  
 
   document.getElementById("ledger-items").innerHTML = ledgerItems.slice(',').join('');
-
+  document.getElementById("total").innerHTML = total;
 };
