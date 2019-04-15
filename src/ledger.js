aa084ad959086417977d00dc518ad4ca5c45cdca
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-04-14 23:50:30 -0400

    Minor styling adjustments to nav

--- src/ledger.js
+++ src/ledger.js
@@ -5,11 +5,11 @@ const buildLedger = root => {
     const ledgerVal = root.children[i].value;
     ledgerItems.push(`<li class="ledger-item">
     <div class="value">${ledgerVal}</div>
-    <span>${root.children[i].data.category}_</span>
+    <span id="cat${i}">${root.children[i].data.category}_</span>
     </li>`);          
   };
   
 
   document.getElementById("ledger-items").innerHTML = ledgerItems.slice(',').join('');
-  document.getElementById("total").innerHTML = root.value;
+  document.getElementById("total").innerHTML = `$${root.value}.00`;
 };
