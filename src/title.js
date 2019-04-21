32ab5496a3d0c0983ff49e1f7590147007c23f8b
Mark Rodriguez <mrkxvncnt@gmail.com>
2019-04-21 13:40:44 -0400

    Incorporate about section into main page

--- src/title.js
+++ src/title.js
@@ -1,15 +1,15 @@
 const enter = () => {
-  document.getElementById("title").classList.add('hidden');
+  document.getElementById("tag-line").classList.add('hidden');
   document.getElementById("app").classList.remove('centered')
   document.getElementById("ledger").classList.remove('hidden');
-  document.getElementById("top-bar").classList.remove('hidden');
+  document.getElementById("nav").classList.remove('hidden');
   document.getElementById("sub-total").classList.remove('hidden');
 }
 
 const exit = () => {
-  document.getElementById("title").classList.remove('hidden');
+  document.getElementById("tag-line").classList.remove('hidden');
   document.getElementById("app").classList.add('centered')
   document.getElementById("ledger").classList.add('hidden');
-  document.getElementById("top-bar").classList.add('hidden');
+  document.getElementById("nav").classList.add('hidden');
   document.getElementById("sub-total").classList.add('hidden');
 }
